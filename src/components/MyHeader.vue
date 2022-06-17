1b1ace3d0a0fb03e99407dc8deaeef9b4af63a35
vince <vincedinardo21@gmail.com>
2022-06-17 17:04:17 +0200

    Prototipo Navbar

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,5 +1,7 @@
 <template>
     <header>
+
+      <!-- Logo & Searchbar Area -->
       <section class="logo-container">
         <div class="container">
           <div class="row">
@@ -15,14 +17,57 @@
             </div>
           </div>            
         </div>
-      </section>           
+      </section> 
+      
+      <!-- Nav Area -->
+      <nav>
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <ul>
+                <li v-for="(item, i) in navBar" :key="i"><a :href="item.link">{{item.text}}</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
     </header>
 </template>
 
 <script>
 export default {
   name: 'MyHeader',
-  
+  data(){
+    return{
+      navBar : [
+        {
+          text : "Home",
+          link : "https://bootstrap-vue.org/docs",
+        },
+        {
+          text : "Shop",
+          link : "#",
+        },
+        {
+          text : "About",
+          link : "#",
+        },
+        {
+          text : "Blog",
+          link : "#",
+        },
+        {
+          text : "Contact",
+          link : "#",
+        },
+        {
+          text : "Shop by brand",
+          link : "#",
+        },
+      ]
+    }
+  }
 }
 </script>
 
