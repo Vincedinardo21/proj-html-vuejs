bdddd12f5d3721164e1f94cdb3bceb604c92035d
vince <vincedinardo21@gmail.com>
2022-06-19 19:37:11 +0200

    Testo jumbotron

--- src/components/MyJumbotron.vue
+++ src/components/MyJumbotron.vue
@@ -3,7 +3,19 @@
         <div class="row">
             <div class="col-12 jumbo-up"></div>
             <!-- Image Background -->
-            <div class="col-12 jumbo-banner"></div>
+            <div class="col-12 jumbo-banner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-6 col-learn">
+                            <div>
+                                <span>FIND THE BEST ANIMAL SUPPLIES</span>
+                                <h2>We know animals are a part of your family, let us help take care of them.</h2>
+                                <div class="btn-learn"><a href="#">Learn more about us</a></div>
+                            </div>                        
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-12"></div>
         </div>
     </div>
@@ -27,5 +39,34 @@ export default {
         background-repeat: no-repeat;
         background-size:cover;
         background-position: center;
+
+        .col-learn {
+            height: 760px;
+            display: flex;
+            align-items: center;
+
+            div {
+                color: white;
+
+                h2 {
+                    font-size: 55px;
+                    margin: 45px 0;
+                }
+            }
+        }
+        .btn-learn {
+            a {
+                background-color: white;
+                color: black;
+                text-decoration: none;
+                padding: 10px 25px;
+                border-radius: 40px;
+
+                &:hover {
+                    background-color: rgb(61, 111, 66);
+                    color: white;
+                }
+            }
+        }
     }
 </style>
